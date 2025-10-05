f6e5841dace2498141c67c29bef21650cdb6fb19
Bless201 <nwagbobiblessing@gmail.com>
2025-10-05 14:57:28 +0100

    Update App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Articles from './pages/Articles';
@@ -11,6 +11,7 @@ import './App.css';
 
 function App() {
   return (
+    <Router>  
       <div className="App">
         <Header />
         <main>
@@ -23,6 +24,7 @@ function App() {
         </main>
         <Footer />
       </div>
+    </Router>
   );
 }
 
